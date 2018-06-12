b1434f68a310caac7760e51f34159415e4915fb4
fam246 <ola.fam10@gmail.com>
2018-06-12 22:55:39 +0100

    Replaced the forEach with map

--- src/components/book-result/BookResults.js
+++ src/components/book-result/BookResults.js
@@ -1,41 +1,55 @@
 import React, { Component } from 'react';
-import {Card, CardHeader, CardText} from 'material-ui/Card';
+import { Card, CardHeader, CardText } from 'material-ui/Card';
 
 class BookResults extends Component {
-  render(){
-    
-    const  {books}  = this.props;
+    render() {
 
-    let bookList = [];
-    Object.keys(books).map((cardIndex) => {
-        let card = books[cardIndex]
-        bookList.push (
-            <Card>
-                <CardHeader
-                    title={card.title}
-                    subtitle={card.author}
-                    actAsExpander={true}
-                    showExpandableButton={true}
-                />
-                <CardText expandable={true}>
-                    { card.description }
-                </CardText>
-            </Card>            
-        )
-    })
 
-    return (
-        
+
+
+        let bookList;
+
+        const books = this.props.books;
+
+        if (books) {
+            bookList = (
+                <Card>
+                    {books.map(book => (
+                        <CardHeader
+                            title={book.title}
+                            subtitle={book.author}
+                            actAsExpander={true}
+                            showExpandableButton={true}
+
+                        />
+                          
+                        
+
+
+
+
+
+                    ))}
+                </Card>
+
+
+            );
+        } else {
+            bookList = null;
+        }
+
+        return (
+
             <div>
-                
-                  
-                    { bookList }
-                
-                
+
+
+                {bookList}
+
+
             </div>
-        
-    );
-}
+
+        );
+    }
 }
 
 
--- src/components/book-search/BookSearch.js
+++ src/components/book-search/BookSearch.js
@@ -6,78 +6,78 @@ import SelectField from 'material-ui/SelectField';
 import BookResults from '../book-result/BookResults';
 
 class Booksearch extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-    this.state ={
-        searchTitle: '',
-        price: 10,
-        apiUrl:'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json',
-        apiKey:'ce9a2049fffb4140a3c3652ba36897f0',
-        books:[]
+        this.state = {
+            searchTitle: '',
+            price: 10,
+            apiUrl: 'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json',
+            apiKey: '',
+            books: []
+        }
+    }
+
+    onTitleChange = e => {
+
+        this.setState({ [e.target.name]: e.target.value }, () => {
+            axios
+                .get(
+                    `${this.state.apiUrl}?api-key=${this.state.apiKey}&title=${
+                    this.state.searchTitle
+                    }&price=${this.state.price}`
+                )
+                .then(res => this.setState({ books: res.data.results }))
+                .catch(err => console.log(err));
+
+
+
+        });
+
+    };
+
+    onPriceChange = (e, index, value) => this.setState({ price: value })
+
+
+
+
+
+    render() {
+        return (
+            <div>
+                <TextField
+                    name="searchTitle"
+                    value={this.state.searchTitle}
+                    onChange={this.onTitleChange}
+                    floatingLabelText="Search For Books"
+                    fullWidth={true}
+                />
+                <br />
+                <SelectField
+                    name="price"
+                    floatingLabelText="Minimum Price"
+                    value={this.state.price}
+                    onChange={this.onPriceChange}
+                >
+
+                    <MenuItem value={10} primaryText="$10" />
+                    <MenuItem value={20} primaryText="$20" />
+                    <MenuItem value={30} primaryText="$30" />
+                    <MenuItem value={40} primaryText="$40" />
+                    <MenuItem value={50} primaryText="$50" />
+
+                </SelectField>
+                <br />
+                {this.state.books.length > 0 ? (
+                    <BookResults books={this.state.books} />
+
+                ) : null}
+
+            </div>
+
+        );
+
     }
 }
 
-onTitleChange = e =>{
 
-this.setState({[e.target.name]: e.target.value},()=>{
-   axios
-   .get (
-    `${this.state.apiUrl}?api-key=${this.state.apiKey}&title=${
-      this.state.searchTitle
-    }&price=${this.state.price}`
-  )
-  .then(res => this.setState({ books: res.data.results }))
-          .catch(err => console.log(err));
-
-
-
-});
-
-};
-
-onPriceChange = (e, index, value ) => this.setState({price: value})
-
-
-
-
-
-       render(){
-           return(
-               <div>
-              <TextField
-              name="searchTitle"
-              value={this.state.searchTitle}
-              onChange={this.onTitleChange}
-              floatingLabelText="Search For Books"
-              fullWidth={true}
-              />
-              <br/>
-              <SelectField
-              name="price"
-              floatingLabelText="Minimum Price"
-              value={this.state.price}
-              onChange={this.onPriceChange}
-              >
-
-            <MenuItem value={10} primaryText="$10" />
-          <MenuItem value={20} primaryText="$20" />
-          <MenuItem value={30} primaryText="$30" />
-          <MenuItem value={40} primaryText="$40" />
-          <MenuItem value={50} primaryText="$50" />
-
-          </SelectField>
-          <br/>
-          {this.state.books.length > 0 ? (
-              <BookResults books={this.state.books}/>
-
-          ) : null}
-        
-         </div>
-
-           );
-
-       }
-}
-
-
-export default  Booksearch;
+export default Booksearch;
